9b1b60e8099c47100e5d47a945ab5f3a919c7fa8
zhangli <zhangli@zhangli-VirtualBox.(none)>
2014-05-22 15:09:04 +0800

    update

--- src/common/config/file.py
+++ src/common/config/file.py
@@ -12,23 +12,30 @@ class FileConfig(object):
         self.params = dict()
         self.params_commentary = dict()
         self.file_path = os.path.abspath(file_path)
-        self._read()
+        if self.exist():
+            self._read()
     
     def _read(self):
         raise NotImplementedError()
     
-    def save(self):
-        # backup original file as temp file
-        temp_file = self.file_path + '_temp'
-        os.rename(self.file_path, temp_file)
-        
-        # save new file
-        self._do_save(temp_file)
-        
-        # remove original file
-        os.remove(temp_file)
+    def exist(self):
+        return os.path.exists(self.file_path) and os.path.isfile(self.file_path)
     
-    def _do_save(self, original_file):
+    def save(self):
+        if self.exist():
+            # backup original file as temp file
+            temp_file = self.file_path + '_temp'
+            os.rename(self.file_path, temp_file)
+            
+            # save new file
+            self._do_save(temp_file)
+            
+            # remove original file
+            os.remove(temp_file)
+        else:
+            self._do_save()
+    
+    def _do_save(self, original_file = None):
         raise NotImplementedError()
     
     def get(self, key):
@@ -58,25 +65,26 @@ class PropertyFileConfig(FileConfig):
                     value = line[split_index + 1:].strip()
                     self.params[key] = value
                     
-    def _do_save(self, original_file):
-        with open(original_file, 'r') as in_f:
-            with open(self.file_path, 'w') as out_f:
-                for line in in_f:
-                    line = line.strip()
-                    split_index = line.find('=')
-                    if not line.startswith('#') and split_index != -1:
-                        key = line[:split_index].strip()
-                        if key in self.params:
-                            value = self.params[key]
+    def _do_save(self, original_file = None):
+        with open(self.file_path, 'w') as out_f:
+            if original_file:
+                with open(original_file, 'r') as in_f:
+                    for line in in_f:
+                        line = line.strip()
+                        split_index = line.find('=')
+                        if not line.startswith('#') and split_index != -1:
+                            key = line[:split_index].strip()
+                            if key in self.params:
+                                value = self.params[key]
+                            else:
+                                continue
+                            
+                            out_f.write("%s=%s\n" % (key, value))
+                            del self.params[key]
                         else:
-                            continue
-                        
-                        out_f.write("%s=%s\n" % (key, value))
-                        del self.params[key]
-                    else:
-                        out_f.write(line + '\n')
+                            out_f.write(line + '\n')
                     
-                for key, value in self.params.iteritems():
-                    if key in self.params_commentary:
-                        out_f.write('#' + self.params_commentary[key] + '\n')
-                    out_f.write("%s=%s\n" % (key, value))
+            for key, value in self.params.iteritems():
+                if key in self.params_commentary:
+                    out_f.write('#' + self.params_commentary[key] + '\n')
+                out_f.write("%s=%s\n" % (key, value))
